2af4de87a1bb06c7bb43d06dc47d85f1a7920158
Athar <atharaimankhan@gmail.com>
2021-12-21 19:18:13 +0500

    problem2 solved

--- problem2.py
+++ problem2.py
@@ -4,8 +4,17 @@
 from utils import print_timed
 
 
-def problem2():
-    raise NotImplementedError("Not implemented yet!")
+def problem2(n1=1, n2=2):
+
+    sum = 0 
+
+    if(n2 > 4000000): return 0
+    sum+= n1 if n1%2==0 else 0
+    sum+= n2 if n2%2==0 else 0
+    
+    return sum + problem2(n1+n2, n1+n2+n2)
+
+        
 
 if __name__ == '__main__':
     print_timed(problem2)
