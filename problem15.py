7a335caa9f3837e74e173ca627f4735d63eae4c4
Athar <atharaimankhan@gmail.com>
2021-12-26 05:31:37 +0500

    problem 25 solved

---
+++ problem15.py
@@ -0,0 +1,34 @@
+"""
+Problem 14:
+URL: https://projecteuler.net/problem=15
+
+"""
+
+
+import itertools
+from utils import print_timed
+from math import factorial
+
+def problem15():
+    
+
+    grid_row= 20
+    grid_col= 20
+    total_steps = grid_row+grid_col        
+    # steps = []
+    # for i in range(grid_row):
+    #     steps.append('R')
+    # for i in range(grid_col):
+    #     steps.append('D')
+    # comb = itertools.permutations(steps,total_steps)
+    # return len(list(set(comb)))
+
+    
+
+    number_of_routes = factorial(total_steps)/(factorial(grid_row)*factorial(total_steps-grid_row))
+    
+    return number_of_routes
+
+if __name__ == '__main__':
+    
+    print_timed(problem15)
