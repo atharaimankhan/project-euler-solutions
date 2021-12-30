d1a4993eb4b8739baaf40eb52372f311c9052deb
Athar <atharaimankhan@gmail.com>
2021-12-30 16:07:51 +0500

    Added some comments in Problem 26

--- problem26.py
+++ problem26.py
@@ -11,32 +11,32 @@ from utils import is_prime, print_timed
 
 def problem26():
     # after researching about this topic i came to know that when its a prime number decimal part will always be recurring. 
-    # Also if d is a prime number then the number of repeating digits is d-1
-    # Example: 1/7 has the 7-1=6 repeating digits   (0.142857......)
-    # So just need to find the largest prime number in the range
+    # Also from wikiepedia, i've found a farmula for finding the recurring numbers which is :
+    #   ((10 ** (prime_number - 1)) - 1) / prime_number
+    # 
     
     max_d = 999
     l = []
 
     for d in range(max_d,6,-1):
         if (is_prime(d)):
-            count_zero = 0
-            for s in str(1/d).split('.')[1]:
+            count_zero = 0                      # problem is that when we use that farmula we dont get the first 0's after the decimal
+            for s in str(1/d).split('.')[1]:    # finding how many zeros are there at the start in decimal part
                 if s != '0':
                     break
                 else:
                     count_zero+=1
             temp = ''
-            if (count_zero >0):
+            if (count_zero >0):                                 
                 temp = '0'*count_zero + str((10**(d-1) -1)//d)    
             else:
                 temp = str((10**(d-1) -1)//d)
             
-            s1 = s[:len(s)//2]
-            s2 = s[len(s)//2:]
+            s1 = temp[:len(temp)//2]
+            s2 = temp[len(temp)//2:]
 
-            if(temp[:len(temp)//2] == temp[len(temp)//2:]):
-                temp = temp[:int(len(temp)/2)]
+            if(s1 == s2):
+                temp = s1
             
             l.append((d, len(temp)))
     
